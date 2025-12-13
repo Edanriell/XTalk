09127f49d855693c8e21aa445418a5bc53075810
Edan <edanriell@gmail.com>
2025-12-13 05:49:39 +0200

    Pkg: Added Register function

---
+++ Server/pkg/health/health.go
@@ -0,0 +1,15 @@
+package health
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+)
+
+// Register adds the standard gRPC Health Checking service to the given server.
+// Call SetServingStatus after the service is ready to accept traffic.
+func Register(server *grpc.Server) *health.Server {
+	hs := health.NewServer()
+	healthpb.RegisterHealthServer(server, hs)
+	return hs
+}
