a30be829761d7a141cdcde8ede91a89bb96b0ee3
Edan <edanriell@gmail.com>
2025-12-14 05:42:42 +0200

    Pkg: Added MutualTLSServerOptions function

--- Server/pkg/grpctls/grpctls.go
+++ Server/pkg/grpctls/grpctls.go
@@ -2,7 +2,9 @@ package grpctls
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -22,6 +24,34 @@ func ServerOptions(certFile, keyFile string) (grpc.ServerOption, error) {
 
 	tlsCfg := &tls.Config{
 		Certificates: []tls.Certificate{cert},
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	return grpc.Creds(credentials.NewTLS(tlsCfg)), nil
+}
+
+// MutualTLSServerOptions returns gRPC server credentials with mutual TLS.
+// Clients must present a certificate signed by the given CA.
+func MutualTLSServerOptions(certFile, keyFile, caFile string) (grpc.ServerOption, error) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, fmt.Errorf("load server cert/key: %w", err)
+	}
+
+	caCert, err := os.ReadFile(caFile)
+	if err != nil {
+		return nil, fmt.Errorf("read CA cert: %w", err)
+	}
+
+	pool := x509.NewCertPool()
+	if !pool.AppendCertsFromPEM(caCert) {
+		return nil, fmt.Errorf("failed to parse CA certificate")
+	}
+
+	tlsCfg := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    pool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
 		MinVersion:   tls.VersionTLS12,
 	}
 
