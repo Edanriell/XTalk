a130c61bc0316d4cb0748c5b08f823c6b42aae36
Edan <edanriell@gmail.com>
2025-12-20 06:05:38 +0200

    Pkg: Added NewGRPCMetrics function

--- Server/pkg/metrics/grpc.go
+++ Server/pkg/metrics/grpc.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 // Registry is a custom Prometheus registry that avoids duplicate registration panics.
@@ -13,4 +14,31 @@ func newRegistry() *prometheus.Registry {
 	r.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 	r.MustRegister(collectors.NewGoCollector())
 	return r
+}
+
+// GRPCMetrics holds Prometheus metrics for gRPC services.
+type GRPCMetrics struct {
+	RequestsTotal   *prometheus.CounterVec
+	RequestDuration *prometheus.HistogramVec
+}
+
+// NewGRPCMetrics registers and returns standard gRPC metrics.
+func NewGRPCMetrics(namespace string) *GRPCMetrics {
+	factory := promauto.With(Registry)
+	return &GRPCMetrics{
+		RequestsTotal: factory.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "grpc",
+			Name:      "requests_total",
+			Help:      "Total number of gRPC requests",
+		}, []string{"method", "code"}),
+
+		RequestDuration: factory.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "grpc",
+			Name:      "request_duration_seconds",
+			Help:      "gRPC request duration in seconds",
+			Buckets:   prometheus.DefBuckets,
+		}, []string{"method"}),
+	}
 }
