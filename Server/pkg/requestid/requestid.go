590a1eeff9bf1e8825fdab3c224413c4a176dc31
Edan <edanriell@gmail.com>
2025-12-29 06:18:35 +0200

    Pkg: Added UnaryServerInterceptor function

--- Server/pkg/requestid/requestid.go
+++ Server/pkg/requestid/requestid.go
@@ -2,6 +2,10 @@ package requestid
 
 import (
 	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type ctxKey struct{}
@@ -19,4 +23,35 @@ func FromContext(ctx context.Context) string {
 // WithRequestID returns a new context with the given request ID.
 func WithRequestID(ctx context.Context, id string) context.Context {
 	return context.WithValue(ctx, ctxKey{}, id)
+}
+
+// UnaryServerInterceptor injects or propagates a request ID in every gRPC call.
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		requestID := extractFromMetadata(ctx)
+		if requestID == "" {
+			requestID = uuid.NewString()
+		}
+		ctx = WithRequestID(ctx, requestID)
+		// Also set it in outgoing metadata for downstream calls
+		ctx = metadata.AppendToOutgoingContext(ctx, MetadataKey, requestID)
+		return handler(ctx, req)
+	}
+}
+
+func extractFromMetadata(ctx context.Context) string {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return ""
+	}
+	vals := md.Get(MetadataKey)
+	if len(vals) == 0 {
+		return ""
+	}
+	return vals[0]
 }
