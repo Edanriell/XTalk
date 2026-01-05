d04e20ae61762a87e7c320d1cfc42e0d420fc14c
Edan <edanriell@gmail.com>
2026-01-05 19:56:54 +0200

    Gateway: handler utils

---
+++ Server/gateway/handlers/utils.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func extractToken(c *gin.Context) string {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		return ""
+	}
+
+	parts := strings.SplitN(authHeader, " ", 2)
+	if len(parts) != 2 || !strings.EqualFold(parts[0], "Bearer") {
+		return ""
+	}
+
+	return parts[1]
+}
