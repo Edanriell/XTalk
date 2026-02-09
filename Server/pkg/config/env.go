4eddf41ae9c1a192e836986e8db902f0466d8b8b
Edan <edanriell@gmail.com>
2026-02-09 09:06:01 +0200

    postgres

--- Server/pkg/config/env.go
+++ Server/pkg/config/env.go
@@ -2,7 +2,6 @@ package config
 
 import "os"
 
-// GetEnv returns the environment variable value or a default.
 func GetEnv(key, defaultValue string) string {
 	if v := os.Getenv(key); v != "" {
 		return v
