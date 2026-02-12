0b59efef8baf03477fb14628a2f971fa328f71fb
Edan <edanriell@gmail.com>
2026-02-12 10:39:17 +0200

    logger

--- Server/pkg/logger/logger.go
+++ Server/pkg/logger/logger.go
@@ -1,3 +1,28 @@
-package main
+package logger
 
-func main() {}
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func New() *zap.Logger {
+	level := zapcore.InfoLevel
+	if lvl := os.Getenv("LOG_LEVEL"); lvl != "" {
+		_ = level.UnmarshalText([]byte(lvl))
+	}
+
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "ts"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderCfg.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderCfg),
+		zapcore.Lock(os.Stdout),
+		level,
+	)
+
+	return zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+}
