41235e9711011550abad027881b62fa8ddd7fbbe
Edan <edanriell@gmail.com>
2026-02-13 10:45:59 +0200

    metrics grpc

---
+++ Server/pkg/metrics/grpc.go
@@ -0,0 +1,44 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var Registry = newRegistry()
+
+func newRegistry() *prometheus.Registry {
+	r := prometheus.NewRegistry()
+
+	r.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+	r.MustRegister(collectors.NewGoCollector())
+
+	return r
+}
+
+type GRPCMetrics struct {
+	RequestsTotal   *prometheus.CounterVec
+	RequestDuration *prometheus.HistogramVec
+}
+
+func NewGRPCMetrics(namespace string) *GRPCMetrics {
+	factory := promauto.With(Registry)
+
+	return &GRPCMetrics{
+		RequestsTotal: factory.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Subsystem: "grpc",
+			Name:      "requests_total",
+			Help:      "Total number of gRPC requests",
+		}, []string{"method", "code"}),
+
+		RequestDuration: factory.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: namespace,
+			Subsystem: "grpc",
+			Name:      "request_duration_seconds",
+			Help:      "gRPC request duration in seconds",
+			Buckets:   prometheus.DefBuckets,
+		}, []string{"method"}),
+	}
+}
