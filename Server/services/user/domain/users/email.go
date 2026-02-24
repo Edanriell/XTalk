5e9e6a17346373388a6a2f3a03745e7e9ccf3e8c
Edan <edanriell@gmail.com>
2026-02-24 20:50:57 +0200

    email

---
+++ Server/services/user/domain/users/email.go
@@ -0,0 +1,18 @@
+package users
+
+import "XTalk/pkg/validation"
+
+type Email struct {
+	value string
+}
+
+func NewEmail(value string) (Email, error) {
+	normalised, err := validation.ValidateEmail(value)
+	if err != nil {
+		return Email{}, err
+	}
+	return Email{value: normalised}, nil
+}
+
+func (e Email) Value() string  { return e.value }
+func (e Email) String() string { return e.value }
