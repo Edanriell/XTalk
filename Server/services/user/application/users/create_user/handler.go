850972be5d63f9321604ae29ef3efbeb297b2674
Edan <edanriell@gmail.com>
2026-03-07 20:56:59 +0200

    create user handler

--- Server/services/user/application/users/create_user/handler.go
+++ Server/services/user/application/users/create_user/handler.go
@@ -1,1 +1,25 @@
 package create_user
+
+import (
+	"context"
+
+	"XTalk/services/user/domain/users"
+)
+
+type Handler struct {
+	userRepo users.UserRepository
+}
+
+func NewHandler(userRepo users.UserRepository) *Handler {
+	return &Handler{userRepo: userRepo}
+}
+
+func (h *Handler) Handle(ctx context.Context, cmd Command) error {
+	email, err := users.CreateEmail(cmd.Email)
+	if err != nil {
+		return err
+	}
+
+	user := users.CreateUser(cmd.UserID, cmd.Username, email, 0, "", "", "")
+	return h.userRepo.Save(ctx, user)
+}
