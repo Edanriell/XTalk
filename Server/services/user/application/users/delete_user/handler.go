000e9a3f85f502a42875bbcc0f1db526f553d673
Edan <edanriell@gmail.com>
2026-03-09 14:04:56 +0200

    delete user handler

--- Server/services/user/application/users/delete_user/handler.go
+++ Server/services/user/application/users/delete_user/handler.go
@@ -1,1 +1,34 @@
 package delete_user
+
+import (
+	"context"
+
+	"XTalk/services/user/domain/users"
+)
+
+type Handler struct {
+	userRepo users.UserRepository
+}
+
+func NewHandler(userRepo users.UserRepository) *Handler {
+	return &Handler{userRepo: userRepo}
+}
+
+func (h *Handler) Handle(ctx context.Context, cmd Command) (*Result, error) {
+	user, err := h.userRepo.FindByID(ctx, cmd.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Deactivate()
+
+	if err := h.userRepo.Save(ctx, user); err != nil {
+		return nil, err
+	}
+
+	return &Result{
+		UserID:  user.ID(),
+		Success: true,
+		Message: "User deleted successfully",
+	}, nil
+}
