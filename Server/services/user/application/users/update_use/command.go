a4851f98e944caf616a7a828b947c8658c8cdac1
Edan <edanriell@gmail.com>
2026-03-13 14:11:56 +0200

    update user command

---
+++ Server/services/user/application/users/update_use/command.go
@@ -0,0 +1,13 @@
+package update_use
+
+type Command struct {
+	UserID    string
+	Username  string
+	Bio       string
+	Age       int
+	Gender    string
+	Country   string
+	Language  string
+	Interests []string
+	AvatarURL string
+}
