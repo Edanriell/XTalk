74b590af495724d5d0029e98129c8aca1febf3e2
Edan <edanriell@gmail.com>
2026-04-08 19:54:15 +0300

    validator

---
+++ Server/services/auth/application/interfaces/validator.go
@@ -0,0 +1,7 @@
+package interfaces
+
+// Validator is an application port for input validation
+type Validator interface {
+	ValidateUsername(username string) error
+	ValidatePassword(password string) error
+}
