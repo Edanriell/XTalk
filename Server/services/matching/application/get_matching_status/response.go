14de89d00baf88154601368df8084fdf3d5e196e
Edan <edanriell@gmail.com>
2026-06-11 11:02:08 +0300

    update

--- Server/services/matching/application/get_matching_status/response.go
+++ Server/services/matching/application/get_matching_status/response.go
@@ -1,1 +1,12 @@
 package get_matching_status
+
+type Response struct {
+	Status      string // "idle", "in_queue", "matched"
+	Message     string
+	WaitTime    int     // seconds (only if in_queue)
+	Priority    int     // (only if in_queue)
+	MatchID     string  // (only if matched)
+	ChatID      string  // (only if matched)
+	MatchedWith string  // (only if matched)
+	MatchScore  float64 // (only if matched)
+}
