af4b68bf28541e0c57c5c6a8eb37d517ffd82f9e
Edan <edanriell@gmail.com>
2026-07-02 11:20:38 +0300

    update

---
+++ Server/services/message/infrastructure/idgen/uuid_generator.go
@@ -0,0 +1,19 @@
+package idgen
+
+import (
+	"github.com/google/uuid"
+	"github.com/yourusername/connect/message-service/application/interfaces"
+)
+
+// UUIDGenerator implements IDGenerator using UUID
+type UUIDGenerator struct{}
+
+// NewUUIDGenerator creates a new UUID generator
+func NewUUIDGenerator() interfaces.IDGenerator {
+	return &UUIDGenerator{}
+}
+
+// Generate generates a new UUID
+func (g *UUIDGenerator) Generate() string {
+	return uuid.New().String()
+}
